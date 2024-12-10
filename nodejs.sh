534f99d9dccee3d51d436886e39f830899af6831
Ubuntu <azureuser@monitor.p2rkbz0tnwqevhvorg1bkdsrce.rx.internal.cloudapp.net>
2024-12-10 05:55:32 +0000

    first commit

--- nodejs.sh
+++ nodejs.sh
@@ -1,3 +1,21 @@
+#install node + npm 
+sudo apt update && sudo apt upgrade -y
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt install -y nodejs
+#pm2 
+sudo npm install -g pm2
+
+npm install
+pm2 start app.js --name my-api
+pm2 startup
+pm2 save
+pm2 logs 0
+pm2 stop nodejs-app
+pm2 restart nodejs-app
+
+
+
+#install NVM
 sudo apt-get update
 
 sudo apt install curl
@@ -10,4 +28,25 @@ nvm install node --lts
 
 nvm use 16
 nvm uninstall 12
+
+#reverse proxy
+'''
+server {
+    listen 80;
+    server_name your-domain.com;
+
+    location / {
+        proxy_pass http://localhost:3000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+    }
+}
+
+'''
+sudo ln -s /etc/nginx/sites-available/nodejs-app /etc/nginx/sites-enabled/
+sudo nginx -t
+sudo systemctl restart nginx
 
